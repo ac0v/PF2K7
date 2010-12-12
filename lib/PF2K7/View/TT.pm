7b8b9dc322f0b3b72c45bc3972e891ff0d153782
Paul Johnson <paul@pjcj.net>
2010-12-13 07:15:25 +0800

    Add TT view.

---
+++ lib/PF2K7/View/TT.pm
@@ -0,0 +1,36 @@
+package PF2K7::View::TT;
+
+use strict;
+use warnings;
+
+use base 'Catalyst::View::TT';
+
+__PACKAGE__->config(
+    TEMPLATE_EXTENSION => '.tt',
+    render_die => 1,
+);
+
+=head1 NAME
+
+PF2K7::View::TT - TT View for PF2K7
+
+=head1 DESCRIPTION
+
+TT View for PF2K7.
+
+=head1 SEE ALSO
+
+L<PF2K7>
+
+=head1 AUTHOR
+
+Paul Johnson,,,
+
+=head1 LICENSE
+
+This library is free software. You can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
+
+1;
