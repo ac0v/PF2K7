6b8d92f271a2a0aa4bb8dda61c63978731165bed
Andreas 'ac0v' Specht <ac0v@sys-network.de>
2010-12-25 03:14:53 +0100

    use formhandler for pf, added validation rules, adjusted code layout

--- lib/PF2K7/Controller/People.pm
+++ lib/PF2K7/Controller/People.pm
@@ -4,6 +4,7 @@ use Moose;
 use namespace::autoclean;
 
 use PF2K7::Form::Login;
+use PF2K7::Form::User::Data;
 
 BEGIN { extends "Catalyst::Controller" }
 
@@ -12,12 +13,25 @@ has form_login => (
     is         => 'ro',
     default    => sub { PF2K7::Form::Login->new(); },
 );
+has form_user_register => (
+    isa        => 'PF2K7::Form::User::Data',
+    is         => 'ro',
+    default    => sub { PF2K7::Form::User::Data->new(); },
+);
+has form_user_profile => (
+    isa        => 'PF2K7::Form::User::Data',
+    is         => 'ro',
+    default    => sub {
+        my $form = PF2K7::Form::User::Data->new();
+        $form->field('submit')->value('Save');
+        return $form;
+    },
+);
 
 sub login :Local :Args(0) {
     my ($self, $c) = @_;
 
     my $form = $self->form_login();
-
 
     # short return if form is not validated == process returns false value
     return if ! $form->process( params => $c->request()->body_parameters() );
@@ -42,39 +56,62 @@ sub login :Local :Args(0) {
     return;
 }
 
-sub logout :Local :Args(0)
-{
+sub logout :Local :Args(0) {
     my ($self, $c) = @_;
 
-    $c->logout;
-    $c->response->redirect($c->uri_for('/'));
+    $c->logout();
+    $c->response()->redirect($c->uri_for('/'));
+
+    return;
 }
 
-sub register :Local :Args(0)
-{
+sub register :Local :Args(0) {
     my ($self, $c) = @_;
 
-    if (lc $c->req->method eq "post")
-    {
-        my $params   = $c->req->params;
-        my $users_rs = $c->model("PF2K7::User");
-        my $newuser  = $users_rs->create
-        ({
-            map { $_ => $params->{$_} }
-                qw( username password name email motto1 motto2 likes dislikes
-                    gps enneagram1 enneagram2 )
-        });
+    # precondition: user is not logged in
+    # ToDo: adjust redirect URL
+    return $c->response()->redirect($c->uri_for('/')) if $c->user();
 
-        unless ($c->authenticate({ username => $params->{username},
-                                   password => $params->{password} }))
-        {
-            $c->stash(message => "Registration failed.");
+    my $form = $self->form_user_register();
+
+    # short return if form is not validated == process returns false value
+    return if ! $form->process( params => $c->request()->body_parameters() );
+
+    # ToDo: add a check for duplicate usernames
+
+    # eval is used to catch all common errors
+    eval {
+        $c->model('PF2K7::User')->create({
+            map {
+                $_ => defined $form->field($_)->value() ? $form->field($_)->value() : q{};
+            } qw( username password name email motto1 motto2 likes dislikes gps
+                  enneagram1 enneagram2 )
+        });
+    };
+
+    if ( ! $@ ) {
+        my $is_authenticated
+            = $c->authenticate({
+                map {
+                    $_ => $form->field($_)->value();
+                } qw(username password)
+              })
+            ;
+
+        if ( $is_authenticated ) {
+            $c->stash(message => 'Registration succeeded.');
+            $c->go('/pf/home');
+
             return;
         }
-
-        $c->stash(message => "Registration succeeded.");
-        $c->go("/pf/home");
     }
+    else {
+        $c->log()->error($@);
+    }
+
+    $c->stash(message => 'Registration Failed.');
+
+    return;
 }
 
 __PACKAGE__->meta->make_immutable;
--- t/100_basic/40controller_People.t
+++ t/100_basic/40controller_People.t
@@ -5,9 +5,10 @@ use warnings;
 
 use Test::More;
 
-use Catalyst::Test "PF2K7";
+use Catalyst::Test 'PF2K7';
 use PF2K7::Controller::People;
 
-ok(request("/people")->is_success, "Request should succeed");
+ok(request('/people/login')->is_success(), 'Request should succeed');
+ok(request('/people/register')->is_success(), 'Request should succeed');
 
 done_testing();
